6a4e2020215cc1e88326f12931fa288c92fdc75e
joao-eduardo17 <125677846+joao-eduardo17@users.noreply.github.com>
2023-05-19 08:13:03 -0300

    site

--- app.py
+++ app.py
@@ -1,37 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 
-app.config['MYSQL_Host'] = 127.0.0.1
+app = Flask("__name__")
+
+app.config['MYSQL_Host'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'contatos'
 
-app = Flask("__name__")
+mysql = MySQL(app)
 
 @app.route("/")
 @app.route("/index")
 def index():
     return render_template('index.html')
-@app.route("/contato")
-def contato():
-    return render_template('contato.html')
+
+#@app.route("/contato")
+#def contato():
+#    return render_template('contato.html')
+
 @app.route("/quemsomos")
 def quemsomos():
     return render_template('quemsomos.html')
-@app.route('/contatos', methods=['GET','POST'])
-def contatos:
+
+@app.route('/contato', methods=['GET','POST'])
+def contato():
     if request.method == 'POST':
-		email = request.form['email']
-		assunto = request.form['assunto']
-		descricao = request.form['descrição']
+        email = request.form['email']
+        assunto = request.form['assunto']
+        descricao = request.form['descrição']
 
-		cur = mysql.connection.cursor()
-		cur.execute('INSERT INTO contatos(email, assunto, descricao) VALUES (%s %s %s)', (email, assunto, descricao))
+        cur = mysql.connection.cursor()
+        cur.execute('INSERT INTO contatos(email, assunto, descricao) VALUES (%s %s %s)', (email, assunto, descricao))
 
-		mysql.connection.commit()
+        mysql.connection.commit()
 
-		cur.close()
+        cur.close()
 
-		return 'Sucesso!'
+        return 'Sucesso!'
+    return render_template('contato.html')
 
-	return render_template('contatos.html')
+@app.route('/users')
+def users():
+    cur = mysql.connection.cursor()
+
+    users = cur.execute("SELECT * FROM contatos")
+
+    if users > 0:
+        userDetails = cur.fetchall()
+
+        return render_template("users.html", userDetails=userDetails)
--- bd-contatos.sql
+++
@@ -1,8 +0,0 @@
-create database contatos;
-use contatos;
-
-create table contatos(
-	email varchar(60),
-    assunto varchar(60),
-    descricao varchar(200)
-);
